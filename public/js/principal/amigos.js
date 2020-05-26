f2a76acf72524dd876475aa457693fe48b79549d
Albson-PC\Albson <andre.tecladist@gmail.com>
2020-05-26 18:52:11 -0300

    Renomeação - Recomendações por Curtir

--- public/js/principal/amigos.js
+++ public/js/principal/amigos.js
@@ -24,44 +24,7 @@ $(document).ready(function () {
 
     //ao clicar em aceitar solicitação de amizade...
     setTimeout(function () {
-        $('.aceitar-solicitacao').on('click', function () {
-            var idAceitado = $(this).attr('data-id-usuario-solicitante');
-            var nomeAceitado = $(this).attr('data-nome-usuario-solicitante');
-            var nomeUsuarioLogado = $('#nome-user-logado-titulo').text();
 
-            var item = $(this).parent().parent().parent().parent().parent().addClass('animated fadeOut');
-
-            $.ajax({
-                url:"/principal/processaAceitacaoAmizade/",
-                method:"POST",
-                data:{idAceitado: idAceitado, nomeAceitado: nomeAceitado, idLogado: userLogado2, nomeLogado: nomeUsuarioLogado},
-                success:function(p){
-                    console.log('idAceitado: ', idAceitado);
-                    console.log('nomeAceitado: ', nomeAceitado);
-                    console.log('idLogado: ', userLogado2);
-                    console.log('nomeLogado: ', nomeUsuarioLogado);
-                    setTimeout(function () {
-                        $(item).remove();
-                        //atualiza status da amizade
-                        $.ajax({
-                            url:"/principal/atualizaStatusAceitacaoAmizade/",
-                            method:"POST",
-                            data:{idAceitado: idAceitado, idLogado: userLogado2},
-                            success:function(p){
-                                $.ajax({
-                                    url:"/principal/refreshListagemAmigos/",
-                                    method:"POST",
-                                    data:{idProprio: userLogado},
-                                    success:function(r){
-                                        $('#content-amigos').html(r);
-                                    }
-                                });
-                            }
-                        });
-                    },300);
-                }
-            });
-        });
     },1000);
 
     //ao clicar em recusar a  solicitação de amizade...
@@ -82,4 +45,46 @@ $(document).ready(function () {
             });
         });
     },1000);
+});
+
+$(window).ready(function () {
+    $('.aceitar-solicitacao').on('click', function () {
+        var idAceitado = $(this).attr('data-id-usuario-solicitante');
+        var nomeAceitado = $(this).attr('data-nome-usuario-solicitante');
+        var nomeUsuarioLogado = $('#nome-user-logado-titulo').text();
+
+        var item = $(this).parent().parent().parent().parent().parent().addClass('animated fadeOut');
+
+        $.ajax({
+            url:"/principal/processaAceitacaoAmizade/",
+            method:"POST",
+            data:{idAceitado: idAceitado, nomeAceitado: nomeAceitado, idLogado: userLogado2, nomeLogado: nomeUsuarioLogado},
+            success:function(p){
+                console.log('idAceitado: ', idAceitado);
+                console.log('nomeAceitado: ', nomeAceitado);
+                console.log('idLogado: ', userLogado2);
+                console.log('nomeLogado: ', nomeUsuarioLogado);
+                setTimeout(function () {
+                    $(item).remove();
+                    //atualiza status da amizade
+                    $.ajax({
+                        url:"/principal/atualizaStatusAceitacaoAmizade/",
+                        method:"POST",
+                        data:{idAceitado: idAceitado, idLogado: userLogado2},
+                        success:function(p){
+                            $.ajax({
+                                url:"/principal/refreshListagemAmigos/",
+                                method:"POST",
+                                data:{idProprio: userLogado},
+                                success:function(r){
+                                    $('#content-amigos').html(r);
+                                }
+                            });
+                        }
+                    });
+                },300);
+            }
+        });
+    });
+    console.log('load script');
 });
