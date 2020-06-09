f9350abb827c62a161d0590ac48386bb33c4bc32
Albson-PC\Albson <andre.tecladist@gmail.com>
2020-06-09 16:24:02 -0300

    Função Comentar

--- App/Views/principal/index.php
+++ App/Views/principal/index.php
@@ -233,7 +233,7 @@
                                                         <!-- <img src="/public/images/liked-img.png" alt=""> -->
 
                                                     </li>
-                                                    <li><a href="#" title="" class="com"><i class="la la-comments"></i> Comentário</a></li>
+                                                    <li><a href="#" title="" class="com"><i class="la la-comments-o"></i> Comentário</a></li>
                                                 </ul>
                                                 <a><i class="la la-eye"></i>Visualizações 0</a>
                                             </div>
